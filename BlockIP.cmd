951af75ffdb72e92ea6c784cede3cd404f69f8b8
Jefta Dirksen <jeftadirksen@gmail.com>
2018-05-14 21:06:06 +0200

    Cleanup temp file

--- BlockIP.cmd
+++ BlockIP.cmd
@@ -2,8 +2,9 @@
 SetLocal EnableDelayedExpansion
 
 :: Get last bad login ip (in last 5 min (300000) )
-wevtutil qe Security /c:1 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 300000]]]" | find "Source Network Address:" > output.tmp
-for /f "tokens=4" %%a in (output.tmp) do (set ip=%%a)
+wevtutil qe Security /c:1 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 300000]]]" | find "Source Network Address:" > BlockIP.tmp
+for /f "tokens=4" %%a in (BlockIP.tmp) do (set ip=%%a)
+del BlockIP.tmp
 echo IP: [%ip%]
 
 :: Ignore local ip addresses
@@ -12,16 +13,18 @@ if not [%iptest%]==[%iptest:b192.168.=%] exit
 if not [%iptest%]==[%iptest:b10.=%] exit
 
 :: Get amount of bad logins from this ip (in last 30 min (1800000) )
-wevtutil qe Security /c:3 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 1800000]]] and *[EventData[Data[@Name='IpAddress'] and (Data='%ip%')]]" | find /c "%ip%" > output.tmp
-for /f "tokens=1" %%a in (output.tmp) do (set count=%%a)
+wevtutil qe Security /c:3 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 1800000]]] and *[EventData[Data[@Name='IpAddress'] and (Data='%ip%')]]" | find /c "%ip%" > BlockIP.tmp
+for /f "tokens=1" %%a in (BlockIP.tmp) do (set count=%%a)
+del BlockIP.tmp
 echo Count: [%count%]
 
 :: Block ip if more than 3 bad attempts
 if /i "%count%" lss "3" exit
 
 :: Get current ip block list
-netsh advfirewall firewall show rule name=BlockIP | find "RemoteIP:" > output.tmp
-for /f "tokens=2" %%a in (output.tmp) do (set ipblocklist=%%a)
+netsh advfirewall firewall show rule name=BlockIP | find "RemoteIP:" > BlockIP.tmp
+for /f "tokens=2" %%a in (BlockIP.tmp) do (set ipblocklist=%%a)
+del BlockIP.tmp
 echo Current ipblocklist: [%ipblocklist%]
 
 :: Add ip to ipblocklist
