a6fa5c3c6b426c818c1d0e97f1f7b537a8d30208
Jefta Dirksen <jeftadirksen@gmail.com>
2018-05-14 21:20:46 +0200

    Formatting

--- BlockIP.cmd
+++ BlockIP.cmd
@@ -1,25 +1,27 @@
 @echo off
 SetLocal EnableDelayedExpansion
+echo Start...
 
 :: Get last bad login ip (in last 5 min (300000) )
 wevtutil qe Security /c:1 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 300000]]]" | find "Source Network Address:" > BlockIP.tmp
 for /f "tokens=4" %%a in (BlockIP.tmp) do (set ip=%%a)
 del BlockIP.tmp
-echo IP: [%ip%]
+if []==[%ip%] goto end
+echo IP address: %ip%
 
 :: Ignore local ip addresses
 set iptest=b%ip%e
-if not [%iptest%]==[%iptest:b192.168.=%] exit
-if not [%iptest%]==[%iptest:b10.=%] exit
+if not [%iptest%]==[%iptest:b192.168.=%] goto localip
+if not [%iptest%]==[%iptest:b10.=%] goto localip
 
 :: Get amount of bad logins from this ip (in last 30 min (1800000) )
 wevtutil qe Security /c:3 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 1800000]]] and *[EventData[Data[@Name='IpAddress'] and (Data='%ip%')]]" | find /c "%ip%" > BlockIP.tmp
 for /f "tokens=1" %%a in (BlockIP.tmp) do (set count=%%a)
 del BlockIP.tmp
-echo Count: [%count%]
+echo Attempts: %count%
 
 :: Block ip if more than 3 bad attempts
-if /i "%count%" lss "3" exit
+if /i "%count%" lss "3" goto end
 
 :: Get current ip block list
 netsh advfirewall firewall show rule name=BlockIP | find "RemoteIP:" > BlockIP.tmp
@@ -32,7 +34,14 @@ set ipblocklist=%ipblocklist%,%ip%
 echo New ipblocklist: [%ipblocklist%]
 
 :: Update firewall rule with new ipblocklist
-echo %date% %time% Blocking ip %ip% >> BlockIP.log
+echo Blocking IP address!
+echo %date% %time% Blocking IP address %ip% >> BlockIP.log
 echo  New ipblocklist: %ipblocklist% >> BlockIP.log
 netsh advfirewall firewall set rule name=BlockIP new remoteip=%ipblocklist%
 if %errorlevel%==1 netsh advfirewall firewall add rule name=BlockIP dir=in action=block remoteip=%ipblocklist%
+
+goto end
+:localip
+echo IP address is local
+:end
+echo Done.
