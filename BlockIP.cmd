b4515fc0e122d6796c55279a10fb92fc1181620a
Jefta Dirksen <jeftadirksen@gmail.com>
2018-05-14 20:50:39 +0200

    First version

---
+++ BlockIP.cmd
@@ -0,0 +1,35 @@
+@echo off
+SetLocal EnableDelayedExpansion
+
+:: Get last bad login ip (in last 5 min (300000) )
+wevtutil qe Security /c:1 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 300000]]]" | find "Source Network Address:" > output.tmp
+for /f "tokens=4" %%a in (output.tmp) do (set ip=%%a)
+echo IP: [%ip%]
+
+:: Ignore local ip addresses
+set iptest=b%ip%e
+if not [%iptest%]==[%iptest:b192.168.=%] exit
+if not [%iptest%]==[%iptest:b10.=%] exit
+
+:: Get amount of bad logins from this ip (in last 30 min (1800000) )
+wevtutil qe Security /c:3 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 1800000]]] and *[EventData[Data[@Name='IpAddress'] and (Data='%ip%')]]" | find /c "%ip%" > output.tmp
+for /f "tokens=1" %%a in (output.tmp) do (set count=%%a)
+echo Count: [%count%]
+
+:: Block ip if more than 3 bad attempts
+if /i "%count%" lss "3" exit
+
+:: Get current ip block list
+netsh advfirewall firewall show rule name=BlockIP | find "RemoteIP:" > output.tmp
+for /f "tokens=2" %%a in (output.tmp) do (set ipblocklist=%%a)
+echo Current ipblocklist: [%ipblocklist%]
+
+:: Add ip to ipblocklist
+set ipblocklist=%ipblocklist%,%ip%
+echo New ipblocklist: [%ipblocklist%]
+
+:: Update firewall rule with new ipblocklist
+echo %date% %time% Blocking ip %ip% >> BlockIP.log
+echo  New ipblocklist: %ipblocklist% >> BlockIP.log
+netsh advfirewall firewall set rule name=BlockIP new remoteip=%ipblocklist%
+if %errorlevel%==1 netsh advfirewall firewall add rule name=BlockIP dir=in action=block remoteip=%ipblocklist%
