ecd168c0217d90be807bc73faff491438e5aa9cf
Jefta Dirksen <JeftaDirksen@gmail.com>
2020-09-25 14:29:10 +0200

    Log and Temp file in script directory

--- BlockIP.cmd
+++ BlockIP.cmd
@@ -1,20 +1,24 @@
 @echo off
 SetLocal EnableDelayedExpansion
+set logfile="%~dp0BlockIP.log"
+set tmpfile=%~dp0BlockIP.tmp
 echo Start...
 
 :: Check parameter for reset
 if "%1"=="reset" goto reset
 
 :: Get last bad login ip (in last 5 min (300000) )
-wevtutil qe Security /c:1 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 300000]]]" | find "Source Network Address:" > BlockIP.tmp
-for /f "tokens=4" %%a in (BlockIP.tmp) do (set ip=%%a)
+wevtutil qe Security /c:1 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 300000]]]" | find "Source Network Address:" > "%tmpfile%"
+echo "%tmpfile%"
+type "%tmpfile%"
+for /f "tokens=4" %%a in (%tmpfile%) do (set ip=%%a)
 echo IP address: %ip%
 if [%ip%]==[] goto end
 if [%ip%]==[-] goto end
 
 :: Get amount of bad logins from this ip (in last 30 min (1800000) )
-wevtutil qe Security /c:3 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 1800000]]] and *[EventData[Data[@Name='IpAddress'] and (Data='%ip%')]]" | find /c "%ip%" > BlockIP.tmp
-for /f "tokens=1" %%a in (BlockIP.tmp) do (set count=%%a)
+wevtutil qe Security /c:3 /rd:true /f:text /q:"*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 1800000]]] and *[EventData[Data[@Name='IpAddress'] and (Data='%ip%')]]" | find /c "%ip%" > "%tmpfile%"
+for /f "tokens=1" %%a in (%tmpfile%) do (set count=%%a)
 echo Attempts: %count%
 
 :: Block ip if more than 3 bad attempts
@@ -27,7 +31,7 @@ if not [%iptest%]==[%iptest:b10.=%] goto localip
 
 :: Create firewall rule for ip
 echo Blocking IP address!
-echo %date% %time% Blocking IP address %ip% >> BlockIP.log
+echo %date% %time% Blocking IP address %ip% >> %logfile%
 netsh advfirewall firewall add rule name=BlockIP dir=in action=block remoteip=%ip%
 
 goto end
@@ -40,8 +44,8 @@ goto end
 
 :localip
 echo IP address is local
-echo %date% %time% Ignoring local ip %ip% >> BlockIP.log
+echo %date% %time% Ignoring local ip %ip% >> %logfile%
 
 :end
-if exist BlockIP.tmp del BlockIP.tmp
+if exist "%tmpfile%" del "%tmpfile%"
 echo Done.
