4b2910d3f2668f3362ea913c58ec0f788e8c28f3
JMDirksen <jeftadirksen@gmail.com>
2022-08-01 10:59:52 +0200

    Reset re-applies the IP block list

--- Reset.cmd
+++ Reset.cmd
@@ -1,4 +1,15 @@
 @echo off
-netsh advfirewall firewall delete rule name=BlockIP
-echo.
-pause
+SetLocal EnableDelayedExpansion
+set blocklistfile="%~dp0BlockIP.list"
+
+call :joinList %blocklistfile%
+netsh advfirewall firewall set rule name=BlockIP new remoteip=%join%
+if %errorlevel% equ 1 netsh advfirewall firewall add rule name=BlockIP dir=in action=block remoteip=%join%
+exit /b
+
+:joinList
+:: Usage: call :joinList "<filename>"
+:: Returns: %join% all the lines from the file joined to one comma separated string
+set join=
+for /f "tokens=*" %%a in ('type %1') do (set join=!join!%%a,)
+exit /b
