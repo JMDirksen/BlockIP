95924b9cae3013997dd6afe3c583c800bc0ba008
JMDirksen <jeftadirksen@gmail.com>
2022-09-19 09:06:06 +0200

    Fix: string (with comma) compare 2

--- Reset.cmd
+++ Reset.cmd
@@ -3,7 +3,7 @@ SetLocal EnableDelayedExpansion
 set blocklistfile="%~dp0BlockIP.list"
 
 call :joinList %blocklistfile%
-if [%join%]==[] netsh advfirewall firewall delete rule name=BlockIP & exit /b
+if "%join%" == "" netsh advfirewall firewall delete rule name=BlockIP & exit /b
 netsh advfirewall firewall set rule name=BlockIP new remoteip=%join%
 if %errorlevel% equ 1 netsh advfirewall firewall add rule name=BlockIP dir=in action=block remoteip=%join%
 exit /b
